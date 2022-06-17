b3cfb80184a0d608837544ab6ab57c477e7e5518
VENKATA RAGURAM KN <107139560+venkata2310@users.noreply.github.com>
2022-06-17 18:11:54 +0530

    Add files via upload

---
+++ type_conversion_practise.py
@@ -0,0 +1,34 @@
+# a=5
+# print(a,type(a)) #5 int
+# print(float(a)) # 5.0
+# print(str(a)) # 5
+# print(int(a)) # 5
+
+# x=17
+# y='python'
+# print(y+str(x)) #python17
+# print(y+' '+str(x)) #python 17
+# print(y,x) #python 17
+# print(y+'  '+('version 3.10.5')) #python  version3.10.5
+
+# z=10.5
+# print(z,type(z))#10.5 <class 'float'>
+# print(str(z))#10.5
+# print(int(z))#10
+
+# w='3'
+# print(w,type(w))
+# print(float(w))
+# print(int(w))
+
+# y='4.5'
+# print(y,type(y))
+# print(float(y))
+# z=float(y)
+# print(int(z))
+
+'''MOST IMPORTANT'''
+print('hel'    +     'lo') #hello
+print('friday'+' '+str(17)+'thJune')
+
+
